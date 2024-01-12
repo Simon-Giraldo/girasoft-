f9e775a004b3817dd91d20cb2ebf314be2ba5654
Simon_Giraldo <simongiraldo562@gmail.com>
2024-01-11 19:24:38 -0500

    se cargan archivos despues de formateo de maquina

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let buttonIniciarSecion = document.getElementById("iniciar_secion")
 let buttonRegistrare = document.getElementById("registrarse")
 
-
 buttonIniciarSecion.addEventListener("click", function() {
     window.location.href = "iniciar_secion.html"
 })
